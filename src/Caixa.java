3851f309e06081aff684ebaee20447b81bc13f94
Gabriel Porto <gsporto226@gmail.com>
2023-01-23 22:05:57 -0300

    Adicionais

--- src/Caixa.java
+++ src/Caixa.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Caixa {
+public class Caixa<T> {
+    private T valor;
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+    public T getValor() {
+        return this.valor;
+    }
+
+    static void exemplo() {
+        // Criei uma caixa de BOOLEAN, só pode boolean
+        // Nesse caso a classe Caixa é genérica em função de Booleans
+        Caixa<Boolean> caixaComBoolean = new Caixa<>();
+        caixaComBoolean.setValor(true);
+        caixaComBoolean.setValor(0); // Erro, não é boolean
+        caixaComBoolean.setValor("não é boolean"); // Erro
+    }
 }
